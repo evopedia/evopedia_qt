66a4cef57e9b23c88072bf1c2f499d3ae516b059
Christian R <christian@evopedia.info>
2010-08-09 01:04:31 +0200

    disk map tile cache, RTL handling, translation, ...
    various bugfixes
    
    webserver:
    handling of RTL languages
    added german translation
    enabled browser cache
    redirection bugfix
    
    map:
    notice about incomplete article display
    better article selection
    disk tile cache (read only for now)

---
+++ src.pri
@@ -0,0 +1,91 @@
+DEFINES += "EVOPEDIA_VERSION=\"\\\"0.4.0\\\"\""
+DEFINES += "EVOPEDIA_WEBSITE=\"\\\"http://evopedia.info\\\"\""
+DEFINES += "EVOPEDIA_DUMP_SITE=\"\\\"http://dumpathome.evopedia.info/dumps/finished\\\"\""
+DEFINES += "EVOPEDIA_BUG_SITE=\"\\\"https://bugs.maemo.org/enter_bug.cgi?product=evopedia\\\"\""
+DEFINES += "MAPTILES_LOCATION=\"\\\"/home/user/MyDocs/.maps\\\"\""
+
+QT += core gui network
+TARGET = evopedia
+TEMPLATE = app
+INCLUDEPATH += src
+SOURCES += src/main.cpp \
+ src/mainwindow.cpp \
+ src/storagebackend.cpp \
+ src/title.cpp \
+ src/titleiterator.cpp \
+ src/titlelistmodel.cpp \
+    src/evopedia.cpp \
+    src/bzreader.cpp \
+    src/evopediawebserver.cpp \
+    src/utils.cpp \
+    src/map.cpp \
+    src/flickable.cpp \
+    src/dumpsettings.cpp \
+    src/mapwindow.cpp \
+    src/flickablemap.cpp
+HEADERS += src/mainwindow.h \
+ src/storagebackend.h \
+ src/title.h \
+ src/titlelistmodel.h \
+ src/titleiterator.h \
+    src/evopedia.h \
+    src/bzreader.h \
+    src/evopediawebserver.h \
+    src/utils.h \
+    src/map.h \
+    src/geotitle.h \
+    src/flickable.h \
+    src/dumpsettings.h \
+    src/mapwindow.h \
+    src/flickablemap.h
+
+TRANSLATIONS += tr/evopedia_de.ts
+
+CONFIG += warn_on
+unix:LIBS += -lbz2
+MOBILITY =
+FORMS += src/mainwindow.ui \
+    src/dumpSettings.ui \
+    src/mapwindow.ui
+DEFINES += QT_NO_CAST_TO_ASCII
+DEFINES += QT_NO_CAST_FROM_BYTEARRAY
+
+OTHER_FILES += \
+    src/evopedia.desktop \
+    src/evopedia.js \
+    src/header.html \
+    src/wikipedia48.png \
+    src/wikipedia.png \
+    src/transtbl.dat \
+    src/random.png \
+    src/maparticle.png \
+    src/map.png \
+    src/main.css \
+    src/magnify-clip.png \
+    src/footer.html
+
+RESOURCES += \
+    src/resources.qrc
+
+unix {
+  #VARIABLES
+  isEmpty(PREFIX) {
+    PREFIX = /usr/local
+  }
+  BINDIR = $$PREFIX/bin
+  DATADIR =$$PREFIX/share
+
+  DEFINES += DATADIR=\\\"$$DATADIR\\\" PKGDATADIR=\\\"$$PKGDATADIR\\\"
+
+  #MAKE INSTALL
+
+  INSTALLS += target desktop iconxpm
+
+  target.path =$$BINDIR
+
+  desktop.path = $$DATADIR/applications/hildon
+  desktop.files += $${TARGET}.desktop
+
+  iconxpm.path = $$DATADIR/pixmaps
+  iconxpm.files += src/wikipedia48.png
+}
---
+++ tr/evopedia_de.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>ArticleOverlay</name>
+    <message>
+        <source>Articles</source>
+        <translation>Artikel</translation>
+    </message>
+</context>
+<context>
+    <name>DumpSettings</name>
+    <message>
+        <source>Dumps</source>
+        <translation>Dumps</translation>
+    </message>
+    <message>
+        <source>Add Dump</source>
+        <translation>Dump hinzufügen</translation>
+    </message>
+    <message>
+        <source>Remove Dump</source>
+        <translation>Dump entfernen</translation>
+    </message>
+    <message>
+        <source>Open Dump Directory</source>
+        <translation>Dump-Verzeichnis öffnen</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Directory %1 does not contain a valid evopedia dump (%2).</source>
+        <translation>Verzeichnis %1 enthält keinen gültigen Evopedia Dump (%2).</translation>
+    </message>
+    <message>
+        <source>%1 (%2), %3 articles</source>
+        <translation>%1 (%2), %3 Artikel</translation>
+    </message>
+</context>
+<context>
+    <name>Evopedia</name>
+    <message>
+        <source>Evopedia Search</source>
+        <translation>Evopedia Suche</translation>
+    </message>
+    <message>
+        <source>Menu</source>
+        <translation>Menü</translation>
+    </message>
+    <message>
+        <source>Configure Dumps</source>
+        <translation>Dumps konfigurieren</translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation>Karte</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über Evopedia</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Unable to store settings.</source>
+        <translation>Konnte Einstellungen nicht speichern.</translation>
+    </message>
+    <message>
+        <source>Error storing settings.</source>
+        <translation>Fehler beim Speichern der Einstellungen.</translation>
+    </message>
+</context>
+<context>
+    <name>FlickableMap</name>
+    <message>
+        <source>Map data CCBYSA 2010 OpenStreetMap.org contributors</source>
+        <translation>Kartendaten CC-BY-SA 2010 OpenStreetMap.org und Beitragende</translation>
+    </message>
+    <message>
+        <source>Zoom in for more articles</source>
+        <translation>Für restliche Artikel hineinzoomen</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>No Dumps Configured</source>
+        <translation>Keine Dumps konfiguriert</translation>
+    </message>
+    <message>
+        <source>To be able to use evopedia you have to download and install a Wikipedia dump. Download at least one dump file from the &lt;a href=&quot;%1&quot;&gt;website&lt;/a&gt; and extract this archive to a folder on your device. After that, select this folder using the menu option &quot;Configure Dumps&quot;.</source>
+        <translation>Um Evopedia benutzen zu können, müssen Sie einen Wikipedia Dump herunterladen und installieren. Laden Sie wenigstens eine Datei von der &lt;a href=&quot;%1&quot;&gt;Webseite&lt;/a&gt; herunter und entpacken Sie das Archiv in einen Ordner auf Ihrem Gerät. Anschließend wählen Sie diesen Ordner über den Menüeintrag &quot;Dumps konfigurieren&quot; aus.</translation>
+    </message>
+    <message>
+        <source>About Evopedia</source>
+        <translation>Über Evopedia</translation>
+    </message>
+    <message>
+        <source>&lt;h2&gt;Evopedia %1&lt;/h2&gt;Offline Wikipedia Viewer&lt;p&gt;&lt;b&gt;Copyright Information&lt;b&gt;&lt;br/&gt;&lt;small&gt;This program shows articles from &lt;a href=&quot;http://wikipedia.org&quot;&gt;Wikipedia&lt;/a&gt;, available under the &lt;a href=&quot;http://creativecommons.org/licenses/by-sa/3.0/&quot;&gt;Creative Commons Attribution/Share-Alike License&lt;/a&gt;. Further information can be found via the links to the online versions of the respective articles.&lt;/small&gt;&lt;/p&gt;</source>
+        <translation>&lt;h2&gt;Evopedia %1&lt;/h2&gt;Offline Wikipedia&lt;p&gt;&lt;b&gt;Urheberrechtsinformationen&lt;b&gt;&lt;br/&gt;&lt;small&gt;Dieses Programm zeigt Artikel aus &lt;a href=&quot;http://wikipedia.org&quot;&gt;Wikipedia&lt;/a&gt;, die unter der &lt;a href=&quot;http://creativecommons.org/licenses/by-sa/3.0/&quot;&gt;Creative Commons Attribution/Share-Alike Lizenz&lt;/a&gt; stehen. Nähere Informationen erhalten Sie über die Links zu den Originalversionen der entsprechenden Artikel.&lt;/small&gt;&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MapWindow</name>
+    <message>
+        <source>Evopedia Map</source>
+        <translation>Evopedia Karte</translation>
+    </message>
+    <message>
+        <source>Menu</source>
+        <translation>Menü</translation>
+    </message>
+    <message>
+        <source>Show Articles</source>
+        <translation>Artikel anzeigen</translation>
+    </message>
+    <message>
+        <source>Zoom In</source>
+        <translation>Hineinzoomen</translation>
+    </message>
+    <message>
+        <source>Zoom Out</source>
+        <translation>Herauszoomen</translation>
+    </message>
+    <message>
+        <source>Can&apos;t grab keys unless we have a window id</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to obtain _HILDON_ZOOM_KEY_ATOM. This example will only work on a Maemo 5 device!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StorageBackend</name>
+    <message>
+        <source>Title file not readable.</source>
+        <translation>Titeldatei nicht lesbar.</translation>
+    </message>
+    <message>
+        <source>Dump directory does not exist.</source>
+        <translation>Dumpverzeichnis existiert nicht.</translation>
+    </message>
+    <message>
+        <source>Metadata file does not exist.</source>
+        <translation>Metadaten-Datei existiert nicht.</translation>
+    </message>
+    <message>
+        <source>Title file does not exist.</source>
+        <translation>Titeldatei existiert nicht.</translation>
+    </message>
+</context>
+</TS>
