03818fc9f8d6898fdac73f591152c7e4a47bcf0f
Christian R <christian@evopedia.info>
2010-08-28 11:38:12 +0200

    Moved some #defines to their own file.

---
+++ src/defines.h
@@ -0,0 +1,9 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define EVOPEDIA_VERSION "0.4.1"
+#define EVOPEDIA_WEBSITE "http://evopedia.info"
+#define EVOPEDIA_DUMP_SITE "http://dumpathome.evopedia.info/dumps/finished"
+#define EVOPEDIA_BUG_SITE "https://bugs.maemo.org/enter_bug.cgi?product=evopedia"
+
+#endif // DEFINES_H
