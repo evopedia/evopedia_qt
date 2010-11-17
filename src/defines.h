551a072503ef03dfe5bc623cf82f5d2354184bbb
Christian R <christian@evopedia.info>
2010-11-17 23:23:46 +0100

    Added Maemo package information.

--- src/defines.h
+++ src/defines.h
@@ -1,7 +1,7 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define EVOPEDIA_VERSION "0.4.1"
+#define EVOPEDIA_VERSION "0.4.2"
 #define EVOPEDIA_WEBSITE "http://evopedia.info"
 #define EVOPEDIA_DUMP_SITE "http://dumpathome.evopedia.info/dumps/finished"
 #define EVOPEDIA_BUG_SITE "https://bugs.maemo.org/enter_bug.cgi?product=evopedia"
