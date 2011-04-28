994c4f3ee41576156e0195922ea9290c26572136
Christian R <christian@evopedia.info>
2011-04-28 14:50:22 +0200

    Changes in metadata for version 0.4.3.

--- src/defines.h
+++ src/defines.h
@@ -21,7 +21,7 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define EVOPEDIA_VERSION "0.4.2"
+#define EVOPEDIA_VERSION "0.4.3"
 #define EVOPEDIA_WEBSITE "http://evopedia.info"
 #define EVOPEDIA_DUMP_SITE "http://dumpathome.evopedia.info/dumps/finished"
 #define EVOPEDIA_BUG_SITE "https://bugs.maemo.org/enter_bug.cgi?product=evopedia"
